d57596370f63d3bbd7a66485a3df9393b3446da0
MatheusCequeira <escrevaparamatheus@gmail.com>
2021-03-10 20:32:56 -0300

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Leitura1;
+            var Leitura2;
+            Leitura1 = Console.Read();
+            Leitura2 = Console.ReadLine();
+            Console.Write(Leitura1);
+            Console.Write(Leitura1);
+            Console.WriteLine(Leitura2);
+            Console.WriteLine(Leitura2);
+        }
+
+    }
+
+}
